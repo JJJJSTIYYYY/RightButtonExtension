1ee5793bf9352d312238ec6013f47ac81ca28017
Justiy <3129424572@qq.com>
2026-01-05 15:17:56 +0800

    Initial Commit

---
+++ RightButtonExtension/RightButtonExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightButtonExtensionApp.swift
+//  RightButtonExtension
+//
+//  Created by Justiy on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct RightButtonExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
